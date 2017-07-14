bcf2d5806a9460ea36280a617e8d7a6c07c9903c
Slim80 <bestboss1980@gmail.com>
2017-07-14 13:54:04 +0200

    Update build and clean scripts and add "fix ramfs permissions" script

--- Build_Imperium_Kernel_aarch64_H811.sh
+++ Build_Imperium_Kernel_aarch64_H811.sh
@@ -10,10 +10,10 @@ export KCONFIG_NOTIMESTAMP=true
 
 IMPERIUM="/home/slim80/Scrivania/Kernel/LG/Imperium"
 KERNELDIR="/home/slim80/Scrivania/Kernel/LG/Imperium/Imperium_Kernel_H811"
-BUILDKERNEL="/home/slim80/Scrivania/Kernel/LG/Imperium/Build_Kernel"
+BUILDKERNEL="/home/slim80/Scrivania/Kernel/LG/Imperium/Build_Kernel_H811"
 FINALKERNEL="/home/slim80/Scrivania/Kernel/LG/Imperium/Final_Kernel"
 IMAGE="/home/slim80/Scrivania/Kernel/LG/Imperium/Imperium_Kernel_H811/arch/arm64/boot"
-ANYKERNEL="/home/slim80/Scrivania/Kernel/LG/Imperium/Imperium_Kernel_H811/AnyKernel"
+RAMFS="/home/slim80/Scrivania/Kernel/LG/Imperium/ramfs_imperium_H811"
 NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
 VERSION=7.1
 
@@ -25,19 +25,13 @@ find -name '*.gz*' -exec rm -rf {} \;
 find -name '*.dtb' -exec rm -rf {} \;
 find -name '*.ko' -exec rm -rf {} \;
 rm -f dt.img
-rm -rf $ANYKERNEL/zImage
-rm -rf $ANYKERNEL/dt.img
-rm -rf $BUILDKERNEL/Slim80/kernel/*
-rm -rf $BUILDKERNEL/system/lib/modules/*
-
-rm -rf ~/.ccache
-ccache -C
+rm -f $BUILDKERNEL/boot.img
+rm -f $BUILDKERNEL/system/lib/modules/*
+rm -f $KERNELDIR/ramfs_imperium_H811.cpio.gz
 
 make ARCH=arm64 imperium_H811_defconfig || exit 1
 
 make -j$NUM_CPUS || exit 1
-
-cp arch/arm64/boot/Image.gz-dtb $ANYKERNEL/zImage
 
 DTB=`find . -name "*.dtb" | head -1`echo $DTB
 echo $DTB
@@ -47,15 +41,18 @@ echo $DTBDIR
 echo $DTBVERCMD
 ./scripts/dtbtoolv3 $DTBVERCMD -o dt.img -s 4096 -p scripts/dtc/ $DTBDIR/
 
-cp dt.img $ANYKERNEL/dt.img
+sh ./fix_ramfs_permissions_H811.sh
+
+cd $RAMFS
+find . | cpio -H newc -o | gzip > $KERNELDIR/ramfs_imperium_H811.cpio.gz
+cd $KERNELDIR
+
+./scripts/mkbootimg --kernel $IMAGE/Image --ramdisk $KERNELDIR/ramfs_imperium_H811.cpio.gz --base 0x00000000 --pagesize 4096 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 --hash sha1 --cmdline 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.selinux=permissive msm_rtb.filter=0x37 androidboot.hardware=p1' --dt dt.img -o $BUILDKERNEL/boot.img
 
 rm -rf imperium_install
 mkdir -p imperium_install
 make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} -j4 INSTALL_MOD_PATH=imperium_install INSTALL_MOD_STRIP=1 modules_install
-find imperium_install/ -name '*.ko' -type f -exec cp '{}' $BUILDKERNEL/system/lib/modules/ \;
-
-cd $ANYKERNEL
-zip -r $BUILDKERNEL/Slim80/kernel/Imperium_Kernel.zip .
+find imperium_install/ -name '*.ko' -type f -exec cp -av {} $BUILDKERNEL/system/lib/modules/ \;
 
 cd $BUILDKERNEL
 zip -r Imperium_Kernel_G4_H811_v$VERSION.zip .
@@ -63,4 +60,4 @@ zip -r Imperium_Kernel_G4_H811_v$VERSION.zip .
 mv ./Imperium_Kernel_G4_H811_v* $FINALKERNEL
 
 echo "* Done! *"
-echo "* Imperium Kernel v$VERSION is ready to be flashad *"
+echo "* Imperium Kernel v$VERSION is ready to be flashed *"
